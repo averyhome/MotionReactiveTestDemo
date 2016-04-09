3f9d804da63a6980c6720c42a08a970b627508e2
朱小亮 <330578304@qq.com>
2016-04-09 17:18:27 +0800

    first commit

---
+++ CoreMotionDemo/motionViewModel.h
@@ -0,0 +1,13 @@
+//
+//  motionViewModel.h
+//  CoreMotionDemo
+//
+//  Created by 朱小亮 on 16/4/9.
+//  Copyright © 2016年 zhusven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface motionViewModel : NSObject
+
+@end
---
+++ CoreMotionDemo/motionViewModel.m
@@ -0,0 +1,13 @@
+//
+//  motionViewModel.m
+//  CoreMotionDemo
+//
+//  Created by 朱小亮 on 16/4/9.
+//  Copyright © 2016年 zhusven. All rights reserved.
+//
+
+#import "motionViewModel.h"
+
+@implementation motionViewModel
+
+@end
